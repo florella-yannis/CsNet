90fbd4464bcdc4498db44531ca32d6b8d6c1ba89
florella-yannis <florellayannis@gmail.com>
2023-04-30 23:13:36 +0200

    modif nav

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,9 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'show_home', methods:['GET'])]
+    #[Route('/', name:'show_home', methods:['GET'])]
     public function showHome(): Response
     {
         return $this->render('default/show_home.html.twig');
+    }
+
+    #[Route('/prestation-entretien', name:'show_entretien', methods:['GET'])]
+    public function showEntretien(): Response
+    {
+        return $this->render('default/show_entretien.html.twig');
+    }
+
+    #[Route('/prestation-espace-vert', name:'show_espace_vert', methods:['GET'])]
+    public function showEspaceVert(): Response
+    {
+        return $this->render('default/show_espace_vert.html.twig');
     }
 }
